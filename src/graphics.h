37086406fe0caddd168c56923eb8da12502f17a2
Arthur Sergeev <sergeevski@outlook.com>
2017-04-23 21:05:48 +0700

    Add graphics function

---
+++ src/graphics.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+void screen_01(int input);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "graphics.h"
+
+int main()
+{
+	int input = 1;
+	screen_01(input);
+	return 0;
+}
