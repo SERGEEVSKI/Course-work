b5b97e40f997a799a5bc4722143b4c13a7b7a9e5
TheAmazingFail-Man <Sheggal@yandex.ru>
2017-05-19 08:01:42 +0700

    Add function group stage

--- all_code/functions.c
+++ all_code/functions.c
@@ -162,3 +162,110 @@ void one_match(commanddata data[], int **teams, int lines, int columns, int onet
 	
 	fclose(matchhistory);*/
 }
+
+void group_stage(commanddata data[], int **teams, int lines, int columns)
+{
+	srand(time(NULL));
+	
+	int error = 0, answer, i, j, z, number, temp, *groupstageteams, *groupstagenumberteams, *finalstagenumberteams, oneteam, twoteam;
+	
+	groupstagenumberteams = (int*)malloc(32 * sizeof(int));
+	groupstageteams = (int*)malloc(32 * sizeof(int));
+	finalstagenumberteams = (int*)malloc(16 * sizeof(int));
+	
+	for (i = 0; i < 32; i++) {
+		groupstagenumberteams[i] = i + 1;
+		groupstageteams[i] = 0;
+	}
+	
+	printf("Group stage 1 or 2(ne robit): ");
+	scanf("%d", &answer);
+	while (answer != 1 && answer != 2) {
+		printf("Error\n");
+		scanf("%d", &answer);
+	}
+	
+	if (answer == 1) { //randomnoe raspredelenie na gruppy
+		for (i = 0; i < 32; i++) {
+			number = rand() % (32 - i);
+			groupstageteams[i] = groupstagenumberteams[number];
+			for (j = number; j < 31; j++) {
+				temp = groupstagenumberteams[j];
+				groupstagenumberteams[j] = groupstagenumberteams[j + 1];
+				groupstagenumberteams[j + 1] = temp;
+			}
+		}
+	} else if (answer == 2) { //samostoyatel'noe raspredelenie na gruppy
+		
+		
+		
+	}
+	
+	for (i = 0; i < 32; i++) {
+		printf("%d ", groupstageteams[i]);
+	}
+	
+	j = 0;
+	
+	for (i = 0; i < 28; i += 4) {
+		oneteam = groupstageteams[i];
+		twoteam = groupstageteams[i + 2];
+		one_match(data, teams, 32, 64, oneteam, twoteam);
+		
+		oneteam = groupstageteams[i + 1];
+		twoteam = groupstageteams[i + 3];
+		one_match(data, teams, 32, 64, oneteam, twoteam);
+		
+		oneteam = groupstageteams[i];
+		twoteam = groupstageteams[i + 1];
+		one_match(data, teams, 32, 64, oneteam, twoteam);
+
+		oneteam = groupstageteams[i + 3];
+		twoteam = groupstageteams[i + 2];
+		one_match(data, teams, 32, 64, oneteam, twoteam);
+
+		oneteam = groupstageteams[i + 3];
+		twoteam = groupstageteams[i];
+		one_match(data, teams, 32, 64, oneteam, twoteam);
+
+		oneteam = groupstageteams[i + 2];
+		twoteam = groupstageteams[i + 1];
+		one_match(data, teams, 32, 64, oneteam, twoteam);
+	}
+	
+	for (z = 0; z <= 28; z += 4) {
+		for (j = 0; j < 3; j++) {
+			for (i = 0 + z; i < 3 + z; i++) {
+				if (data[groupstageteams[i] - 1].points < data[groupstageteams[i + 1] - 1].points) {
+					temp = groupstageteams[i];
+					groupstageteams[i] = groupstageteams[i + 1];
+					groupstageteams[i + 1] = temp;
+				}
+			}
+		}
+	}
+	
+	printf("\n");
+	
+	for (i = 0; i < 32; i++) {
+		printf("%d ", groupstageteams[i]);
+	}
+	
+	j = 0;
+	for (i = 0; i < 32; i += 4) {
+		finalstagenumberteams[j++] = groupstageteams[i];
+		finalstagenumberteams[j++] = groupstageteams[i + 1];
+	}
+	
+	printf("\n");
+	
+	for (i = 0; i < 16; i++) {
+		printf("%d ", finalstagenumberteams[i]);
+	}
+	
+	return;
+	
+	system("pause");
+	
+//	final_stage(data, teams, 32, 64, finalstagenumberteams);
+}
--- all_code/main.c
+++ all_code/main.c
@@ -1,25 +1,30 @@
-#include "functions.h"
+include "functions.h"
 
 int main(void)
 {
+	FILE *matchhistory = fopen("matchhistory.txt", "w");
+	fclose(matchhistory);
+	
 	setlocale(LC_ALL, "Rus");
 	
 	commanddata data[32];
 	
-	int i = 0, j, oneteam, twoteam;
+	int answer,  i = 0, j, oneteam, twoteam, n, m, winnerteam;
 	
-	int  **teams = (int**)calloc(18, sizeof(int*));
-	for (i = 0; i < 18; i++) {
-		teams[i] = (int*)calloc(18, sizeof(int));
+	int  **teams = (int**)calloc(2048, sizeof(int*));
+	for (i = 0; i < 2048; i++) {
+		teams[i] = (int*)calloc(2048, sizeof(int));
     }
-	FILE *probability = fopen("test.txt", "r");
-	for (i = 0; i < 3; i++) {
-		for(j = 0; j < 6; j++) {
+    
+	FILE *probability = fopen("data.txt", "r");
+	for (i = 0; i < 32; i++) {
+		for (j = 0; j < 64; j++) {
 			fscanf(probability, "%d", &teams[i][j]);
 		}
 	}
 	fclose(probability);
 	
+	i = 0;
 	FILE *commandnames = fopen("teams.txt", "r");
 	while (!feof(commandnames)) {
 		fgets(data[i].name, 50, commandnames);
@@ -27,11 +32,55 @@ int main(void)
 	}
 	fclose(commandnames);
 	
-	scanf("%d", &oneteam);
-	scanf("%d", &twoteam);
-	one_match(data, teams, 32, 64, oneteam, twoteam);
+	printf("play(1) or help(2)(error) or exit(3): ");
 	
-//	system("pause");
+	scanf("%d", &answer);
+	while (answer != 1 && answer != 2 && answer != 3) {
+		printf("Error\n");
+		scanf("%d", &answer);
+	}
+	
+	if (answer == 1) {
+		
+		printf("liga(1) or one match(2): ");
+		
+		scanf("%d", &answer);
+		while (answer != 1 && answer != 2) {
+			printf("Error\n");
+			scanf("%d", &answer);
+		}
+		
+		if (answer == 1) {
+			group_stage(data, teams, 32, 64);
+			
+			return;
+		} else if (answer == 2) {
+			
+			printf("one team and two team: ");
+			
+			scanf("%d", &oneteam);
+			while (oneteam < 1 || oneteam > 32) {
+				printf("Error\n");
+				scanf("%d", &oneteam);
+			}
+			scanf("%d", &twoteam);
+			while (twoteam < 1 || twoteam > 32 || twoteam == oneteam) {
+				printf("Error\n");
+				scanf("%d", &twoteam);
+			}
+			winnerteam = one_match(data, teams, 32, 64, oneteam, twoteam);
+			
+			return;
+		}
+		
+	} else if (answer == 2) {
+		
+	} else if (answer == 3) {
+		
+		return;
+	}
+	
+	system("pause");
 	
 	return 0;
 }
