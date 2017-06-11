ad4bcd8b33fd7dac3f3f3b2182340fcf8675c4b9
Arthur Sergeev <sergeevski@outlook.com>
2017-06-12 01:25:07 +0700

    Delete some details in function

--- src/graphics.h
+++ src/graphics.h
@@ -1,8 +1,1 @@
-#include <stdio.h>
 
-#ifndef GRAPHICS_H
-#define GRAPHICS_H
-
-void screen_01(int input);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,11 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "graphics.h"
 
-int main()
-{
-	int input = 1;
-	screen_01(input);
-	return 0;
-}
